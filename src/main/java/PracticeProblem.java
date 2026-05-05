47e68b39f2d383a5109e654129fc08d4e502f075
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-05-05 16:49:52 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,39 @@
+/*
+File: Lesson 5.4 - Arrays, split, swapping
+Author: Stephen Li
+Date Created: May 4, 2026
+Date Last Modified: May 5, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-}
+	public static void stringSwap(String[] array1, int index1, int index2) {
+		String temp = array1[index1];
+		array1[index1] = array1[index2];
+		array1[index2] = temp;
+	}
+
+	public static void reverse(int[] array1) {
+		int[] array2 = new int[array1.length];
+		for (int i = 0; i < array1.length; i++) {
+			array2[i] = array1[array1.length - 1 - i];
+		}
+		for (int i = 0; i < array1.length; i++) {
+			array1[i] = array2[i];
+		}
+	}
+
+	public static String[] toWordArray(String word1) {
+		word1 = word1.trim();
+		if (word1.equals("")) {
+			return new String[0];
+		}
+		else {
+			return word1.split(" +");
+		}
+	}
+} 
